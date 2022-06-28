814adcd14b4efb8ff463202e0d078329b0376cc9
Alexander Moryan <a.dencyh@gmail.com>
2022-06-28 16:09:13 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import "./assets/styles.css";
+
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+const toTableButton = document.querySelector("#toTable");
+const toRowsButton = document.querySelector("#toRows");
+//const table = document.querySelector("#table");
+const tbody = document.querySelector("#tbody");
+
+input.value = ` а664кх797 х128еу797, а658кн797, р871ем797
+
+`;
+
+toTableButton.addEventListener("click", (e) => {
+  try {
+    let raw = input.value;
+
+    let string = raw.replace(/\,/gm, " ").replace(/\s{2,}/gm, " ");
+
+    if (string == undefined) return;
+    console.log(string);
+
+    let names = string.match(
+      /[А-ЯA-Z]{2,}\s[А-ЯA-Z]{2,}(\s*[А-Я]{2,})*(\s[А-Я]{2,})*/gim
+    );
+
+    let numbers = string.match(/([А-ЯA-Z]{1}[0-9]{3}[А-ЯA-Z]{2}[0-9]{0,3})/gim);
+
+    /* Phone numbers */
+
+    let phones = string
+      .match(/(\+7|7|8)*\d{3}\s*\d{3}\s*\d{2}\s*\d{2}(?=($|\s|[А-Я]))/gim)
+      .map((item) => {
+        return item.replace(/(\s|\+)/g, "");
+      });
+    console.log(phones);
+
+    names.forEach((name, index) => {
+      tbody.innerHTML += `<tr>
+    <td class="col0">${index}</td>
+    <td class="col1">${numbers[index]}</td>
+    <td class="col2"></td>
+    <td class="col3">${name}</td>
+    <td class="col4">${phones[index]}</td>
+    </tr>`;
+    });
+  } catch (e) {
+    console.error(e);
+    output.value = "Ошибка, придется делать по старинке";
+  }
+});
+
+toRowsButton.addEventListener("click", () => {
+  try {
+    let raw = input.value;
+    let string = raw
+      .replace(/\,/gm, " ")
+      .replace(/\s{2,}/gm, " ")
+      .replace(/\s/g, "\n")
+      .replace(/^\s/, "");
+    output.value = string;
+  } catch (e) {
+    console.error(e);
+  }
+});
